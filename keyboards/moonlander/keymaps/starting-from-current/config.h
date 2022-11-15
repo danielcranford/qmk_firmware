02b63e14b73ba9c1a8c3d4dedbfa9591b7fe680c
dcranford <daniel.cranford@red-alpha.com>
2022-11-15 13:19:14 -0500

    adding current keymap

---
+++ keyboards/moonlander/keymaps/starting-from-current/config.h
@@ -0,0 +1,16 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef DEBOUNCE
+#define DEBOUNCE 5
+
+#define ONESHOT_TAP_TOGGLE 3
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"gn0QN/vQvBR"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define RGB_MATRIX_STARTUP_SPD 60
