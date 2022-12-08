00bc7c35baa801797abba8c93b3e121896170d2a
dcranford <daniel.cranford@red-alpha.com>
2022-12-08 09:45:48 -0500

    Why is RGBLIGHT_MODE_STATIC_LIGHT undefined in keymap.c? Also IGNORE_MOD_TAP_INTERRUPT has been missing for who knows how long.

--- keyboards/moonlander/keymaps/combos/config.h
+++ keyboards/moonlander/keymaps/combos/config.h
@@ -3,6 +3,7 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
 #undef DEBOUNCE
 #define DEBOUNCE 5
 
